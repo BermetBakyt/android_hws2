761ea87500a381eafadd175d6c5d414c31094ca6
Bermet Bakytbekova <bembakyt@gmail.com>
2021-12-29 18:31:31 +0600

    hw 32

--- app/src/main/java/com/ber/android_hws/MainActivity.kt
+++ app/src/main/java/com/ber/android_hws/MainActivity.kt
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ber.android_hws.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         val recycler = binding.recycler
         recycler.adapter = adapter
-        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.layoutManager = GridLayoutManager(this, 2)
         recycler.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
 
         val list = mutableListOf<String>()
