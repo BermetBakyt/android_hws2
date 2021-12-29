20db5b3d21292904e2d5b43f5832736df1c3af02
Bermet Bakytbekova <bembakyt@gmail.com>
2021-12-29 17:52:21 +0600

    hw 31 finished

--- app/src/main/java/com/ber/android_hws/Fragment1.kt
+++ app/src/main/java/com/ber/android_hws/Fragment1.kt
@@ -1,35 +1,17 @@
 package com.ber.android_hws
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 
 class Fragment1: Fragment(R.layout.fragment_1) {
-    private lateinit var listener: OnButtonClicked
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        listener = context as OnButtonClicked
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btn1 = view.findViewById<AppCompatButton>(R.id.btn_1)
-        val btn2 = view.findViewById<AppCompatButton>(R.id.btn_2)
-        val btn3 = view.findViewById<AppCompatButton>(R.id.btn_3)
-    }
-    private fun translateIdToIndex(id:Int): Int {
-        var index = -1
-        when (id) {
-            R.id.btn_1 -> index = 1
-            R.id.btn_2 -> index = 2
-            R.id.btn_3 -> index = 3
-        }
-        return index
-    }
-    fun onClick(v: View?) {
-        val  buttonIndex = translateIdToIndex(v!!.id)
     }
 }
+
--- app/src/main/java/com/ber/android_hws/MainActivity.kt
+++ app/src/main/java/com/ber/android_hws/MainActivity.kt
@@ -1,32 +1,55 @@
 package com.ber.android_hws
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import com.bumptech.glide.Glide
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(), OnButtonClicked {
+class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initFragments()
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.nav_home -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, Fragment1())
+                    .commit()
+                //drawerLayout.closeDrawer(GravityCompat.START)
+                R.id.nav_message -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, Fragment2())
+                    .commit()
+
+            }
+            toggle.syncState()
+            true
+
+
+        }
 
     }
-    private fun initFragments() {
-        val fragment1 = supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container1, Fragment1())
-            .commit()
-        val fragment2 = supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container2, Fragment2())
-            .commit()
-    }
 
-    override fun onClick(buttonIndex: Int) {
-        val fragment2 = supportFragmentManager.findFragmentById(R.id.fragment_container2) as Fragment2?
-        fragment2.setImage(buttonIndex)
-    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
+        if (toggle.onOptionsItemSelected(item)) {
+
+            return true
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
-
