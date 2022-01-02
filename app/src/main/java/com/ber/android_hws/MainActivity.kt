c7a5573f9bdff42b62e3368bf4ecdf540968290a
Bermet Bakytbekova <bembakyt@gmail.com>
2022-01-02 23:58:16 +0600

    hw 36

--- app/src/main/java/com/ber/android_hws/MainActivity.kt
+++ app/src/main/java/com/ber/android_hws/MainActivity.kt
@@ -1,30 +1,31 @@
 package com.ber.android_hws
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var input: AppCompatEditText
     private lateinit var text: AppCompatTextView
+    private lateinit var input: AppCompatEditText
     private lateinit var btn: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        input = findViewById(R.id.edit)
         text = findViewById(R.id.text)
+        input = findViewById(R.id.edit)
         btn = findViewById(R.id.btn)
 
         btn.setOnClickListener {
-
             text.text = input.text.toString().split("\\P{L}+".toRegex())
-                .filter { it.count { it == 'o'} == 1 }
+                .filter { it.endsWith("ая") }
                 .size
                 .toString()
         }
     }
+
+
 }
